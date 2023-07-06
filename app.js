31c892e82d5971f75695e7c21821dcfb2afc0509
jannasherrill <127800933+jannasherrill@users.noreply.github.com>
2023-07-06 17:47:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let maximum = parseInt(prompt("Enter the highest number!"));
+let maximum = parseInt(prompt("Enter the maximum number!"));
 while(!maximum){
     maximum = parseInt(prompt("Enter a valid number!"));
 }
 const targetNum = Math.floor(Math.random()* maximum) + 1;
-console.log(targetNum)
+
 
 let guess = parseInt(prompt("Enter your first guess!"));
 let attempts = 1;
