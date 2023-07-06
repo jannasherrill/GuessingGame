26c6cb2773a59da2e4cf3b4a00beb69f935fad80
jannasherrill <127800933+jannasherrill@users.noreply.github.com>
2023-07-06 17:17:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let maximum = parseInt(prompt("Enter the highest number!"));
-
+while(!maximum){
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
 const targetNum = Math.floor(Math.random()* maximum) + 1;
 console.log(targetNum)
