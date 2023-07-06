93e4e7ec8a598a207031ae8c2b0bb58d6bc25457
jannasherrill <127800933+jannasherrill@users.noreply.github.com>
2023-07-06 17:39:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ while(!maximum){
 }
 const targetNum = Math.floor(Math.random()* maximum) + 1;
 console.log(targetNum)
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+
+while (parseInt(guess) !==targetNum) {
+    if(guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = parseInt(prompt("Too high! Enter a new guess:"));
+    } else {
+        guess = parseInt(prompt("Too low! Enter a new guess:"));
+    }
+
+    }
+}
+if (guess === 'q'){
+    console.log("Ok, you quit!")
+} else { 
+}
+console.log (`You Got It Dude! It took you ${attempts} guesses`)
