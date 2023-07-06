db18065ae0f6923fb80a01b9d806950ab9b04a87
jannasherrill <127800933+jannasherrill@users.noreply.github.com>
2023-07-06 17:03:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let maximum = parseInt (prompt("Enter the highest number!"));
+let maximum = parseInt(prompt("Enter the highest number!"));
 
-const targetNum = Math.floor(Math.random()* maximum) +1;
+const targetNum = Math.floor(Math.random()* maximum) + 1;
 console.log(targetNum)
